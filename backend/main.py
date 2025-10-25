60d912d7892e6d4a427c1b0787257bdf15cfb5f5
sislam11 <sislam11@students.cs.ubc.ca>
2025-10-25 13:52:05 -0700

    Added fastapi skeleton

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,13 @@
-from google import genai
-from dotenv import load_dotenv
-import os
-from supabase import create_client, Client
+from fastapi import FastAPI
+from services.gemini_client import ask_gemini
 
-load_dotenv()
+app = FastAPI()
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+@app.get("/")
+def hello_world():
+    return {"message": "Hello, world!"}
 
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
-
-response2 = (
-    supabase.table("test")
-    .select("*")
-    .execute()
-)
-
-print(response2)
+@app.get("/ask")
+def ask_ai(prompt: str):
+    answer = ask_gemini(prompt)
+    return {"prompt": prompt, "answer": answer}
